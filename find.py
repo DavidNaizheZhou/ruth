e5cbbf85390babf2deda81402d14eac808d5bb6f
DavidNaizheZhou <david.zhou@tuwien.ac.at>
2023-01-01 14:15:18 +0100

    Update find.py
    comments

--- find.py
+++ find.py
@@ -3,27 +3,51 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.metrics import r2_score
 
+# loop through all files in data
 for file in os.listdir("data"):
+
     print(f"File: {file}")
+
+    # load Data from .csv
     data = np.genfromtxt(os.path.join("data", file), delimiter=";")
+
+    # extract cols
     x = data[:, 0]
     y = data[:, 1]
 
+    # fit 1D polynomial to data
     model = np.polyfit(x, y, 1)
+
+    # generate prediction function with coeff.
     predict = np.poly1d(model)
+
+    # calculate R² Score
     r2 = r2_score(y, predict(x))
 
+    # check and set color
     if r2 >= 0.9:
         color = "green"
     else:
         color = "red"
 
+    # min, max for range
     min, max = np.min(x), np.max(x)
+
+    ## plotting
     fig, ax = plt.subplots()
+
+    # loaded data
     ax.scatter(x, y, s=4, c="black")
+
+    # xpoints for regression line
     x_plot = np.linspace(min, max, 100)
+
+    # plot regression line
     ax.plot(x_plot, predict(x_plot), c=color, label=f"y = {model[0]:.3f} x + {model[1]:.3f}\nR²={r2:.3f}")
+
     ax.legend()
     ax.grid()
     ax.set_axisbelow(True)
+
+    # save the figures to the folder plots
     plt.savefig(os.path.join("plots", f"{file.replace('.csv', '')}.png"))
